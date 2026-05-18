389b2e8cc9b854e58a35c4c9f0697e5b343ac530
yudi240180180-cmyk <yudi.240180180@mhs.unimal.ac.id>
2026-05-18 21:11:59 +0700

    feat: implement database seeder to auto-generate 100 random users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,38 +4,25 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use App\Models\Room;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // 1. Membuat 4 user utama terlebih dahulu
-        $user1 = User::factory()->create(['name' => 'Budi', 'email' => 'budi@gmail.com']);
-        $user2 = User::factory()->create(['name' => 'Siti', 'email' => 'siti@gmail.com']);
-        $user3 = User::factory()->create(['name' => 'Andi', 'email' => 'andi@gmail.com']);
-        $user4 = User::factory()->create(['name' => 'Rose', 'email' => 'rose@gmail.com']);
+        // 1. Buat satu Room Grup Utama bawaan sistem sebelum user dibuat
+        Room::create(['name' => 'Grup Diskusi Tugas', 'type' => 'group']);
 
-        $userMasal = User::factory(50)->create();
+        // 2. Membuat 4 user utama untuk login pengujian
+        User::factory()->create(['name' => 'Budi', 'email' => 'budi@gmail.com']);
+        User::factory()->create(['name' => 'Siti', 'email' => 'siti@gmail.com']);
+        User::factory()->create(['name' => 'Andi', 'email' => 'andi@gmail.com']);
+        User::factory()->create(['name' => 'Rose', 'email' => 'rose@gmail.com']);
 
-        $allUsers = User::all();
-
-        $groupRoom = Room::create(['name' => 'Grup Diskusi Tugas', 'type' => 'group']);
-        
-        $groupRoom->users()->attach($allUsers->pluck('id'));
-
-        $userCount = $allUsers->count();
-        for ($i = 0; $i < $userCount; $i++) {
-            for ($j = $i + 1; $j < $userCount; $j++) {
-                $privateRoom = Room::create(['type' => 'private']);
-                $privateRoom->users()->attach([$allUsers[$i]->id, $allUsers[$j]->id]);
-            }
-        }
+        // 3. Membuat 100 akun massal acak agar grup chat langsung ramai
+        User::factory(100)->create();
     }
 }
