d12c59f122c8295ad7f1eb4405986cf3b352f3c6
yudi240180180-cmyk <yudi.240180180@mhs.unimal.ac.id>
2026-05-18 21:27:46 +0700

    fix: merge dynamic chat routes and restore core profile routes to fix crash

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ChatController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -11,15 +12,18 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-use App\Http\Controllers\ChatController;
-
 Route::middleware(['auth'])->group(function () {
+    
     Route::get('/chat', [ChatController::class, 'index'])->name('chat.index');
     Route::get('/chat/{id}', [ChatController::class, 'show'])->name('chat.show');
     Route::post('/chat/{id}', [ChatController::class, 'sendMessage'])->name('chat.send');
     
     Route::post('/chat/private/create', [ChatController::class, 'createPrivateChat'])->name('chat.private.create');
     Route::post('/chat/group/create', [ChatController::class, 'createGroupChat'])->name('chat.group.create');
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';
